b6a63d77d5d8f760763446beab9c1a337581893c
YuzuruSitra <yuzurusitra@gmail.com>
2023-11-16 01:54:03 +0900

    Map sizing and camera manager implementation.

---
+++ Assets/Dev_Yuzu/CamManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CamManager : MonoBehaviour
+{
+    [SerializeField]
+    CinemachineVirtualCameraBase _vcamRight;
+    [SerializeField]
+    CinemachineVirtualCameraBase _vcamCenter;
+    [SerializeField]
+    CinemachineVirtualCameraBase _vcamLeft;
+    private GameObject _targetPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_targetPlayer == null) _targetPlayer = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_targetPlayer.transform.position.x <= -3.5f)
+        {
+            _vcamLeft.Priority = 1;
+            _vcamRight.Priority = 0;
+            _vcamCenter.Priority = 0;
+        }
+        else if(_targetPlayer.transform.position.x >= 3.5f)
+        {
+            _vcamLeft.Priority = 0;
+            _vcamRight.Priority = 1;
+            _vcamCenter.Priority = 0;
+        }
+        else
+        {
+            _vcamLeft.Priority = 0;
+            _vcamRight.Priority = 0;
+            _vcamCenter.Priority = 1;
+        }
+    }
+}
