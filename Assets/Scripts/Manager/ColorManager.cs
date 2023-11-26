41e03390ef3a034c2b6ef2deffa6d23003679a2a
kaikai-git <kaiandika@icloud.com>
2023-11-26 21:35:11 +0900

    Up

--- Assets/Scripts/Manager/ColorManager.cs
+++ Assets/Scripts/Manager/ColorManager.cs
@@ -4,47 +4,47 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
-    UIHandler _uiHandler;
-    //色を変える対象のオブジェクト
-    [SerializeField] GameObject _blockPrefab;
+  //   UIHandler _uiHandler;
+  //   //色を変える対象のオブジェクト
+  //   [SerializeField] GameObject _blockPrefab;
   
 
-    GameObject _blockInstance;
-    void Start()
-    {
-         _uiHandler = GameObject.FindWithTag("UIHandler").GetComponent<UIHandler>();
+  //   GameObject _blockInstance;
+  //   void Start()
+  //   {
+  //        _uiHandler = GameObject.FindWithTag("UIHandler").GetComponent<UIHandler>();
 
-         //プレハブからインスタンスを生成
-          _blockInstance = Instantiate(_blockPrefab);
-       // CreateBlock();
-    }
+  //        //プレハブからインスタンスを生成
+  //         _blockInstance = Instantiate(_blockPrefab);
+  //      // CreateBlock();
+  //   }
 
-   //ブロックに色を割り当てる（ボタンを押したとき）
-   //ここを後でオブジェクトから参照したマテリアルにする処理に変更
-   public void SetRedBrockMaterial()
-   {
-       //インスタンスのRendererから共有マテリアルにアクセス
-         _blockInstance.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color",Color.red);
-         _uiHandler.ActiveFalseButton();
-   }
-   public void SetBlueBrockMaterial()
-   {
-       //インスタンスのRendererから共有マテリアルにアクセス
-         _blockInstance.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color",Color.blue);
-         _uiHandler.ActiveFalseButton();
-   }
-   public void SetGreenBrockMaterial()
-   {
-       //インスタンスのRendererから共有マテリアルにアクセス
-         _blockInstance.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color",Color.green);
-          _uiHandler.ActiveFalseButton();
-   }
-   public void SetYellowBrockMaterial()
-   {
-       //インスタンスのRendererから共有マテリアルにアクセス
-         _blockInstance.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color",Color.yellow);
-         _uiHandler.ActiveFalseButton();
-   }  
+  //  //ブロックに色を割り当てる（ボタンを押したとき）
+  //  //ここを後でオブジェクトから参照したマテリアルにする処理に変更
+  //  public void SetRedBrockMaterial()
+  //  {
+  //      //インスタンスのRendererから共有マテリアルにアクセス
+  //        _blockInstance.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color",Color.red);
+  //        _uiHandler.ActiveFalseButton();
+  //  }
+  //  public void SetBlueBrockMaterial()
+  //  {
+  //      //インスタンスのRendererから共有マテリアルにアクセス
+  //        _blockInstance.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color",Color.blue);
+  //        _uiHandler.ActiveFalseButton();
+  //  }
+  //  public void SetGreenBrockMaterial()
+  //  {
+  //      //インスタンスのRendererから共有マテリアルにアクセス
+  //        _blockInstance.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color",Color.green);
+  //         _uiHandler.ActiveFalseButton();
+  //  }
+  //  public void SetYellowBrockMaterial()
+  //  {
+  //      //インスタンスのRendererから共有マテリアルにアクセス
+  //        _blockInstance.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color",Color.yellow);
+  //        _uiHandler.ActiveFalseButton();
+  //  }  
 
    
 }
