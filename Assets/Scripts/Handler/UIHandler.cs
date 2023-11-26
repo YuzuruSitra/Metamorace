6b8a01ae070b0473f34ef91db8bd057bfecbea2b
kaikai-git <kaiandika@icloud.com>
2023-11-26 20:33:56 +0900

    Up

--- Assets/Scripts/Handler/UIHandler.cs
+++ Assets/Scripts/Handler/UIHandler.cs
@@ -29,7 +29,7 @@ public class UIHandler : MonoBehaviour
 
     public void GetItemEffect()
     {
-        itemeffect.Animator()
+       // itemeffect.Animator()
     }
     public void ShowLimitTime(float _TimeLimit)
     {
