3c001d0943a5740ea2d8abc8d85cd623abdb5557
kaikai-git <kaiandika@icloud.com>
2023-11-27 18:17:33 +0900

    Playerwwait

--- Assets/Scripts/Player/Player_Wait.cs
+++ Assets/Scripts/Player/Player_Wait.cs
@@ -29,10 +29,12 @@ public class Player_Wait : MonoBehaviour
     private bool _animJump = false;
     private bool _animWalk = false;
     private bool _animIdole = false;
-   
+   private SoundHandler _soundHandler;
+   [SerializeField] AudioClip jump;
 
     void Start()
     {
+         _soundHandler = SoundHandler.InstanceSoundHandler;
         _rb = GetComponent<Rigidbody>();
         _myPV = GetComponent<PhotonView>();
     }
@@ -65,8 +67,15 @@ public class Player_Wait : MonoBehaviour
     void PlayerCtrl()
     {
         float inputX = Input.GetAxis("Horizontal");
-        if(inputX == 0) return;
-
+        if(inputX == 0)
+        {
+            _animIdole = true;
+            _animWalk = false;
+            Debug.Log(_animIdole);
+            return;
+        }
+        _animIdole = false;
+        _animWalk = true;
         // カメラの方向を考慮して移動ベクトルを作成
         Vector3 forward = Camera.main.transform.forward;
         forward.y = 0.0f;
@@ -85,20 +94,40 @@ public class Player_Wait : MonoBehaviour
 
     }
 
-    void Jump()
+   void Jump()
     {
+        //if(_animSwing) return;
+        float raypos = 0.45f;
+        float rayheight = 0.1f;
         // Jump handling
-        //後で綺麗にします
-        Ray ray = new Ray(transform.position,new Vector3(0,-_jumprayrength,0));
-        RaycastHit hit;
-        Debug.DrawRay(transform.position,Vector3.down * _jumprayrength, Color.red, 0.1f); 
-        if (Physics.Raycast(ray, out hit,_jumprayrength))  _isJump = false;
-        else _isJump = true;
-
-        if (Input.GetKeyDown(KeyCode.Space) && _isJump == false)
-        {         
+        bool _isJump = CheckAndJump(new Ray(transform.position + new Vector3(raypos, rayheight, raypos), Vector3.down)) ||
+                        CheckAndJump(new Ray(transform.position + new Vector3(-raypos, rayheight, -raypos), Vector3.down)) ||
+                        CheckAndJump(new Ray(transform.position + new Vector3(raypos, rayheight, -raypos), Vector3.down)) ||
+                        CheckAndJump(new Ray(transform.position + new Vector3(-raypos, rayheight, raypos), Vector3.down));
+        if (Input.GetKeyDown(KeyCode.Space) && _isJump)
+        {
+            //ジャンプSE鳴らす
+            _soundHandler.PlaySE(jump);
             _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    private bool CheckAndJump(Ray ray)
+    {
+        RaycastHit hit;
+        // Debug.DrawRay(transform.position, Vector3.down * _jumprayrength, Color.red, 0.1f); 
+        Debug.DrawRay(ray.origin, ray.direction * _jumprayrength, Color.red, 0.1f);
+        if (Physics.Raycast(ray, out hit, _jumprayrength))
+        {
+            _isJump = false;
+            _animJump = false;
+            return true; // 地面に当たっている場合はtrueを返す
+        }
+        else
+        {
             _isJump = true;
+            _animJump = true;
+            return false; // 地面に当たっていない場合はfalseを返す
         }
     }
 
@@ -135,6 +164,7 @@ public class Player_Wait : MonoBehaviour
            _playerAnim.SetBool("_isJump", false);
             _playerAnim.SetBool("_isIdole", true);
             _playerAnim.SetBool("_isWalk", false);
+            _playerAnim.SetFloat("MoveSpeed", 0.0f);
         }
         else if(_animJump)
         {
@@ -144,9 +174,10 @@ public class Player_Wait : MonoBehaviour
         }
         else if(_animWalk)
         {
-            _playerAnim.SetBool("_isStan", false);           
+            _playerAnim.SetBool("_isJump", false);           
             _playerAnim.SetBool("_isIdole", false);
             _playerAnim.SetBool("_isWalk", true);
+            _playerAnim.SetFloat("MoveSpeed", 1.0f);
         }
     }
 
