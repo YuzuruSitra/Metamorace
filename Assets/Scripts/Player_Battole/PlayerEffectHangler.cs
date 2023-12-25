086af728cd972491c80393261865d191ef0ce1b8
YuzuruSitra <yuzurusitra@gmail.com>
2023-12-25 15:56:34 +0900

    エフェクトの修正

--- Assets/Scripts/Player_Battole/PlayerEffectHangler.cs
+++ Assets/Scripts/Player_Battole/PlayerEffectHangler.cs
@@ -8,26 +8,38 @@ public class PlayerEffectHangler : MonoBehaviour
     private PlayerDataReceiver _playerDataReceiver;
     [SerializeField] 
     private GameObject _staneffect;
+    [SerializeField]
+    private Animator _stanEffectAnim;
+
     [SerializeField] 
     private GameObject _saiyaeffect;
-    [SerializeField]
-    private Animator _stanEffect;
-    [SerializeField]
-    private float _stanpos = 0.3f;
+    private const float EFFECT_POS_Z = 0.4f;
+    private Vector3 staneOffSetTeam0 = new Vector3(0, 0.8f, -EFFECT_POS_Z);
+    private Vector3 staneOffSetTeam1 = new Vector3(0, 0.8f, EFFECT_POS_Z);
+    private Vector3 saiyaOffSetTeam0 = new Vector3(0, 1.2f, -EFFECT_POS_Z);
+    private Vector3 saiyaOffSetTeam1 = new Vector3(0, 1.2f, EFFECT_POS_Z);
     private bool _animStan = false;
     public bool AnimStan => _animStan;
 
-
-    // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        if (_playerDataReceiver.MineTeamID == 0) _staneffect.transform.position = transform.position + new Vector3(0, 0, -_stanpos);
-        else _staneffect.transform.position = transform.position + new Vector3(0, 0, _stanpos);
+        // エフェクト移動
+        if(_staneffect.activeSelf)
+        {
+            if (_playerDataReceiver.MineTeamID == 0) _staneffect.transform.position = transform.position + staneOffSetTeam0;
+            else _staneffect.transform.position = transform.position + staneOffSetTeam1;
+        }
+        
+        if (_saiyaeffect.activeSelf)
+        {
+            if (_playerDataReceiver.MineTeamID == 0) _saiyaeffect.transform.position = transform.position + saiyaOffSetTeam0;
+            else _saiyaeffect.transform.position = transform.position + saiyaOffSetTeam1;
+        }
     }
 
     public void ChangeStan(bool state)
     {
-        _stanEffect.SetBool("Stan", state);
+        _stanEffectAnim.SetBool("Stan", state);
         _animStan = state;
     }
 
